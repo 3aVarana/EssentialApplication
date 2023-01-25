2491fe8b0d71c7ffee2317d225229879e0d618be
Andres Arana <3av.arana@gmail.com>
2023-01-25 10:00:09 -0400

    Create `configureWindow` method in `SceneDelegate`

---
+++ EssentialApplicationTests/SceneDelegateTests.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegateTests.swift
+//  EssentialApplicationTests
+//
+//  Created by Victor Arana on 1/25/23.
+//
+
+import XCTest
+import Feed_Project_iOS
+@testable import EssentialApplication
+
+final class SceneDelegateTests: XCTestCase {
+
+    func test_sceneWillConnectToSession_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindow()
+
+        sut.configureWindow()
+
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+        XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+
+}
